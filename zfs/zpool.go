178c5786bff13991d0309df7aaa29e12cb909f85
guojl <guojl@hualu.com.cn>
2019-10-24 11:12:22 +0800

    zpool status 解析

--- zfs/zpool.go
+++ zfs/zpool.go
@@ -6,18 +6,19 @@ import (
 )
 
 type DevEntry struct {
-	name   string
-	state  string
-	read   string
-	write  string
-	cksum  string
-	rest   string
-	level  int //级别,标识设备层次关系
-	device int //1:是最底层设备
+	Name   string
+	State  string
+	Read   string
+	Write  string
+	Cksum  string
+	Rest   string
+	Level  int //级别,标识设备层次关系
+	Device int //1:是最底层设备
 }
 
 func poolConfParse(confstr string) (devs []DevEntry, err error) {
 	minIndent := 0
+	afterSpares := 0
 	for _, origline := range strings.Split(confstr, "\n") {
 		if origline == "" {
 			continue
@@ -39,38 +40,49 @@ func poolConfParse(confstr string) (devs []DevEntry, err error) {
 			minIndent = indent
 			continue
 		}
+		if f[0] == "spares" {
+			afterSpares = 1
+		}
 
 		var dev DevEntry
-		dev.level = indent - minIndent
-		dev.device = 0
+		dev.Level = indent - minIndent
+		dev.Device = 0
 
 		if lenf > 0 {
-			dev.name = f[0]
+			dev.Name = f[0]
 		}
 		if lenf > 1 {
-			dev.state = f[1]
+			dev.State = f[1]
 		}
-		if lenf > 2 {
-			dev.read = f[2]
+
+		if afterSpares == 1 {
+			if lenf > 2 {
+				dev.Rest = strings.Join(f[2:], " ")
+			}
+		} else {
+			if lenf > 2 {
+				dev.Read = f[2]
+			}
+			if lenf > 3 {
+				dev.Write = f[3]
+			}
+			if lenf > 4 {
+				dev.Cksum = f[4]
+			}
+			if lenf > 5 {
+				dev.Rest = strings.Join(f[5:], " ")
+			}
 		}
-		if lenf > 3 {
-			dev.write = f[3]
-		}
-		if lenf > 4 {
-			dev.cksum = f[4]
-		}
-		if lenf > 5 {
-			dev.rest = strings.Join(f[5:], " ")
-		}
-		if dev.level > 0 {
-			lowerName := strings.ToLower(dev.name)
+
+		if dev.Level > 0 {
+			lowerName := strings.ToLower(dev.Name)
 			if !strings.Contains(lowerName, "mirror") &&
-				//!strings.Contains(lowerName, "spares") &&
 				//!strings.Contains(lowerName, "cache") &&
 				//!strings.Contains(lowerName, "logs") &&
 				!strings.Contains(lowerName, "raidz") &&
+				!strings.Contains(lowerName, "spare") &&
 				!strings.Contains(lowerName, "replacing") {
-				dev.device = 1
+				dev.Device = 1
 			}
 		}
 		devs = append(devs, dev)
@@ -79,6 +91,24 @@ func poolConfParse(confstr string) (devs []DevEntry, err error) {
 }
 
 /*
+
+	confStr = `
+			config:
+
+			NAME              STATE     READ WRITE CKSUM
+			pool2             DEGRADED     0     0     0
+			  mirror-0        DEGRADED     0     0     0
+			    replacing-0   DEGRADED     0     0     0
+			      wwn-0x6660  UNAVAIL      0     0     0 cannot open
+			      wwn-0x6634  ONLINE       0     0     0 8M resilvered
+			    wwn-0x6668    ONLINE       0     0     0
+			cache
+			  wwn-0x66c0      ONLINE       0     0     0
+			spares
+			  wwn-0x66c2      INUSE     currently in use
+
+			`
+
 {"name":"pool2","state":"DEGRADED","read":"0","write":"0","cksum":"0","rest":"","level":"0","device":"0"}
 {"name":"mirror-0","state":"DEGRADED","read":"0","write":"0","cksum":"0","rest":"","level":"1","device":"0"}
 {"name":"replacing-0","state":"DEGRADED","read":"0","write":"0","cksum":"0","rest":"","level":"2","device":"0"}
@@ -88,7 +118,8 @@ func poolConfParse(confstr string) (devs []DevEntry, err error) {
 {"name":"cache","state":"","read":"","write":"","cksum":"","rest":"","level":"0","device":"0"}
 {"name":"wwn-0x66c0","state":"ONLINE","read":"0","write":"0","cksum":"0","rest":"","level":"1","device":"1"}
 {"name":"spares","state":"","read":"","write":"","cksum":"","rest":"","level":"0","device":"0"}
-{"name":"wwn-0x66c2","state":"AVAIL","read":"","write":"","cksum":"","rest":"","level":"1","device":"1"}
+{"name":"wwn-0x66c2","state":"INUSE","read":"","write":"","cksum":"","rest":"currently in use","level":"1","device":"1"}
+
 */
 func main() {
 	var confStr string
@@ -96,19 +127,19 @@ func main() {
 	confStr = `
 		config:
 
-		NAME              STATE     READ WRITE CKSUM
-		pool2             DEGRADED     0     0     0
-		  mirror-0        DEGRADED     0     0     0
-		    replacing-0   DEGRADED     0     0     0
-		      wwn-0x6660  UNAVAIL      0     0     0 cannot open
-		      wwn-0x6634  ONLINE       0     0     0 8M resilvered
-		    wwn-0x6668    ONLINE       0     0     0
+		NAME                                          STATE     READ WRITE CKSUM
+		pool2                                         ONLINE       0     0     0
+		  mirror-0                                    ONLINE       0     0     0
+		    wwn-0x66c92bf0003a1d0224dea98414160c34    ONLINE       0     0     0
+		    spare-1                                   ONLINE       0     0     0
+		      wwn-0x66c92bf0003a1d0224dea9040c76f760  ONLINE       0     0     0
+		      wwn-0x66c92bf0003a1d0224dea9b316e0e5c2  ONLINE       0     0     0
 		cache
-		  wwn-0x66c0      ONLINE       0     0     0
+		  wwn-0x66c92bf0003a1d0224dea9d518f253c0      ONLINE       0     0     0
 		spares
-		  wwn-0x66c2      AVAIL
+		  wwn-0x66c92bf0003a1d0224dea9b316e0e5c2      INUSE     currently in use
 
-		`
+	`
 
 	devs, err := poolConfParse(confStr)
 	if err != nil {
@@ -116,7 +147,7 @@ func main() {
 	} else {
 		for _, dev := range devs {
 			devStr := fmt.Sprintf(`{"name":"%s","state":"%s","read":"%s","write":"%s","cksum":"%s","rest":"%s","level":"%d","device":"%d"}`,
-				dev.name, dev.state, dev.read, dev.write, dev.cksum, dev.rest, dev.level, dev.device)
+				dev.Name, dev.State, dev.Read, dev.Write, dev.Cksum, dev.Rest, dev.Level, dev.Device)
 			fmt.Println(devStr)
 		}
 	}
