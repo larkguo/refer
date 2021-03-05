6dfb39ad302bea82f1e6232d43efb82804c2b7f5
larkguo <larkguo@gmail.com>
2021-03-05 18:08:53 +0800

    Update downloader.go
    add retry when error

--- http/downloader.go
+++ http/downloader.go
@@ -18,7 +18,8 @@ type FileDownloader struct {
 	writer       io.WriterAt // 接口,实现Write方法
 	fileSize     int64       // 文件大小
 	url          string      //下载地址
-	concurrency  int64       // 并发数
+	concurrency  int         // 并发数
+	maxRetries   int         // 重试次数
 	doneFilePart []FilePart  // 所有数据分片结构
 }
 type FilePart struct {
@@ -34,13 +35,13 @@ func (part *FilePart) Write(p []byte) (n int, err error) {
 		return 0, io.EOF
 	}
 	cur := part.from + part.cur
-	n, err = part.writer.WriteAt(p, cur) // 每段数据按顺序多次回调,需要记录当前位置
+	n, err = part.writer.WriteAt(p, cur) //每段数据按顺序多次回调,需要记录当前位置
 	part.cur += int64(n)
-
 	log.Printf("job[%v] get range[%v-%v) \n", part.id, cur, part.cur)
 	return
 }
-func NewFileDownloader(url string, concurrency int64) *FileDownloader {
+
+func NewFileDownloader(url string, concurrency, retries int) *FileDownloader {
 	file, err := os.Create(filepath.Base(url))
 	if err != nil {
 		return nil
@@ -50,11 +51,13 @@ func NewFileDownloader(url string, concurrency int64) *FileDownloader {
 		url:          url,
 		writer:       file,
 		concurrency:  concurrency,
+		maxRetries:   retries,
 		doneFilePart: make([]FilePart, concurrency),
 	}
 }
+
 func (down *FileDownloader) head() (int64, error) {
-	r, err := http.NewRequest("HEAD", down.url, nil) // 获取文件大小等元信息
+	r, err := http.NewRequest("HEAD", down.url, nil) //获取文件大小等元信息
 	if err != nil {
 		return 0, err
 	}
@@ -65,12 +68,13 @@ func (down *FileDownloader) head() (int64, error) {
 	if resp.StatusCode > 299 {
 		return 0, errors.New(fmt.Sprintf("Can't process, %v", resp.Status))
 	}
-	if resp.Header.Get("Accept-Ranges") != "bytes" { // 不支持分段
+	if resp.Header.Get("Accept-Ranges") != "bytes" { //不支持分段
 		return 0, errors.New("Server not support of partial requests")
 	}
 	n, err := strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 64)
 	return n, err
 }
+
 func (down *FileDownloader) Run() error {
 	fileSize, err := down.head()
 	if err != nil {
@@ -78,50 +82,53 @@ func (down *FileDownloader) Run() error {
 	}
 	down.fileSize = fileSize
 
-	jobs := make([]FilePart, down.concurrency)
-	pageSize := fileSize / down.concurrency // 分片大小
+	parts := make([]FilePart, down.concurrency)
+	pageSize := fileSize / int64(down.concurrency) //分片大小
 
-	for i := range jobs { // 创建多个任务
-		jobs[i].id = i
+	for i := range parts { //创建多个任务
+		parts[i].id = i
 		if i == 0 {
-			jobs[i].from = 0
+			parts[i].from = 0
 		} else {
-			jobs[i].from = jobs[i-1].to + 1
+			parts[i].from = parts[i-1].to + 1
 		}
 		if i < int(down.concurrency-1) {
-			jobs[i].to = jobs[i].from + pageSize
+			parts[i].to = parts[i].from + pageSize
 		} else {
-			jobs[i].to = fileSize - 1 // 最后一片
+			parts[i].to = fileSize - 1 //最后一片
 		}
 	}
 
 	var wg sync.WaitGroup
-	for _, j := range jobs { // 多个任务同时下载
+	for _, part := range parts { //多片同时下载
 		wg.Add(1)
-		go func(job FilePart) {
+		go func(p FilePart) {
 			defer wg.Done()
-			_, err := down.downPart(&job) // 分段分页下载
-			if err != nil {
-				log.Println("downPart:", err, job)
+			for retry := 0; retry <= down.maxRetries; retry++ {
+				_, err := down.downPart(&p) // 分段分页下载
+				if err == nil {
+					break
+				}
 			}
-		}(j)
+		}(part)
 	}
-	wg.Wait() // 所有任务执行完成
+
+	wg.Wait() //所有任务执行完成
 	return nil
 }
-func (d *FileDownloader) downPart(part *FilePart) (int64, error) {
+func (d *FileDownloader) downPart(part *FilePart) (n int64, err error) {
 	r, err := http.NewRequest("GET", d.url, nil)
 	if err != nil {
 		return 0, err
 	}
-	part.writer = d.writer // 继承writer
+	part.writer = d.writer //继承writer
 	r.Header.Set("Range", fmt.Sprintf("bytes=%v-%v", part.from, part.to))
 	resp, err := http.DefaultClient.Do(r)
 	if err != nil {
 		return 0, err
 	}
 	defer resp.Body.Close()
-	n, err := io.Copy(part, resp.Body) // 回调FilePart.Write()
+	n, err = io.Copy(part, resp.Body) //回调FilePart.Write()
 	if err != nil {
 		return 0, err
 	}
@@ -129,17 +136,14 @@ func (d *FileDownloader) downPart(part *FilePart) (int64, error) {
 }
 func main() {
 	startTime := time.Now()
-
 	url := flag.String("u", "https://mojotv.cn/go/go-range-download", "http(s)://... ")
-	jobs := flag.Int64("j", 3, "Concurrency jobs ")
+	jobs := flag.Int("j", 3, "Concurrency jobs ")
+	retries := flag.Int("r", 3, "MaxRetries ")
 	flag.Parse()
-	if *url == "" {
-		fmt.Println("Specify a Url(-u 'http(s)://...' ) Jobs (-j JOBS) ")
-		return
-	}
-	downloader := NewFileDownloader(*url, *jobs)
+	downloader := NewFileDownloader(*url, *jobs, *retries)
 	if err := downloader.Run(); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("Took: %f second\n", time.Now().Sub(startTime).Seconds())
 }
+
